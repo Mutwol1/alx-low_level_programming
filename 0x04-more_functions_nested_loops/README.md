283452e1978b4a840516d3c3c9429482443256b8
Godfrey Mutwol <mutgodfrey@gmail.com>
2022-10-25 14:06:35 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,2 +1,1 @@
- 0x04-more_functions_nested_loops
-
+0x04. C - More functions, more nested loops
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,11 +1,11 @@
-#ifndef FILE_MAIN
-#define FILE_MAIN
+#ifndef MAIN_H
+#define MAIN_H
 
-int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
+int _putchar(char);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
